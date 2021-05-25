a53eca84c340465d2409033b2e6b19633954bf91
Joseph Cumines <joeycumines@gmail.com>
2021-05-25 15:02:02 +1000

    update modules

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/codegangsta/cli"
+	"github.com/urfave/cli"
 
 	"github.com/joeycumines/gotty/backend/localcommand"
 	"github.com/joeycumines/gotty/pkg/homedir"
